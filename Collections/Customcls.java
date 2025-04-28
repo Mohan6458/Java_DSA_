714b2f2a5f94ccafd8971f35a8a9db1cf0afdc18
mohan6458 <mohanakrishnan85670@gmail.com>
2025-04-28 16:51:51 +0530

    Custom Classes

--- Collections/Customcls.java
+++ Collections/Customcls.java
@@ -1,6 +1,29 @@
 package Collections;
 
 class Data{
+    public int num;
+    public String name;
+
+    Data(int _num,String _name){
+        this.name=_name;
+        this.num=_num;
+    }
+}
+public class Customcls {
+    public static void main(String[] args) {
+        Data dataObj1= new Data(6,"mohan");
+        Data dataObj2= new Data(7,"krishnan");
+        dataObj1.name="mohaaan";
+        System.out.println(dataObj2.name);
+
+
+
+    }
+    
+}
+
+/*
+ * lass Data{
     private int num;
     private String name;
 
@@ -33,3 +56,5 @@ public class Customcls {
     }
     
 }
+
+ */
