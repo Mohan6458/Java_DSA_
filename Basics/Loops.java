023831a32d8b70842036331e30b73f8152c26526
mohan6458 <mohanakrishnan8670@gmail.com>
2025-05-22 00:12:01 +0530

    Learning Core Java

---
+++ Basics/Loops.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Loops {
+
+    public static void areaofrectangle(int length,int width ){
+        int area = length * width;
+        System.out.println(area);
+
+    }
+    public static void main(String[] args) {
+
+        areaofrectangle(10,10);
+        
+
+
+
+
+        // String[] name = {"Mohan","Nikhil","Barath"};
+        // int[] marks ={50,60,70};
+        // int highest = marks[0];
+        // String topscoreName = name[0];
+
+        // for(int i=0;i<name.length;i++){
+        //     if(marks[i]>highest){
+        //         highest = marks[i];
+        //         topscoreName = name[i];
+                
+        //     }
+
+        // }
+        // System.out.println("Names is " + topscoreName + " and  mark is " + highest);
+
+
+
+
+
+
+        // Scanner scanner = new Scanner(System.in);
+
+        // System.out.println("Enter The Name: ");
+        // String name = scanner.next();
+
+        // System.out.println(name.length());
+        // System.out.println(name.toUpperCase());
+        // System.out.println(name.contains("Mohan"));
+
+
+
+    //     int n=1;
+
+    //     while(n<=100){
+    //         System.out.println(n);
+    //         n++;
+    //     }
+        //for(int i=0;i<=n;i++){
+        //    System.out.println(i);
+        //}
+    }
+    
+}
